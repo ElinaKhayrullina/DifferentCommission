4578ab2c46b7ce46e2f79b57b9e2972b12f0db90
Elina <shanonn@mail.ru>
2025-07-03 22:25:05 +0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val com = commission(85000, "Mastercard", 150000)
+    println(com)
+}
+
+val dailyLimit = 150_000
+val monthlyLimit = 600_000
+val monthlyLimitMastercard = 75_000
+val commisionMastercard = 0.006
+val addCommissinMastercard = 20
+val commisionVisa = 0.0075
+val minCommissionVisa = 35
+fun commission(transaction: Int, card: String = "Мир", allTransactions: Int = 0): String {
+    return when {
+        (transaction > dailyLimit || allTransactions + transaction > monthlyLimit) -> "Превышен лимит переводов"
+        (card == "Mastercard" && transaction <= monthlyLimitMastercard) -> "Комиссия с $transaction рублей не взимается"
+        (card == "Mastercard" && transaction >= monthlyLimitMastercard) -> "Комиссия с $transaction рублей составит ${(transaction - monthlyLimitMastercard) * commisionMastercard + addCommissinMastercard} рублей"
+        card == "Visa" && (transaction * commisionVisa) < minCommissionVisa -> "Комиссия с $transaction рублей составит 35 рублей"
+        card == "Visa" && (transaction * commisionVisa) >= minCommissionVisa -> "Комиссия с $transaction рублей составит ${transaction * commisionVisa} рублей"
+        else -> "Комиссия с $transaction рублей не взимается"
+    }
+}
